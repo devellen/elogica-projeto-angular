ecc751048facb440fd9b408a12c00520470b1e9f
devellen <ellenpatrycya66@gmail.com>
2025-04-15 16:15:34 -0300

    feat: criar estrutura de pastas e arquivos

---
+++ questao11/memoteca/src/app/pags/mural/mural.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mural',
+  standalone: true,
+  imports: [],
+  templateUrl: './mural.component.html',
+  styleUrl: './mural.component.css'
+})
+export class MuralComponent {
+
+}
