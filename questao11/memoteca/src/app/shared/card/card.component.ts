c0f4fcdc731e352684fc5a9f55fed78fca881673
devellen <ellenpatrycya66@gmail.com>
2025-04-16 14:43:28 -0300

    feat: implementar componente de card com estilizacao dinamica com o modelo

---
+++ questao11/memoteca/src/app/shared/card/card.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './card.component.html',
+  styleUrl: './card.component.css'
+})
+export class CardComponent {
+  @Input() modelo : number = 0;
+  @Input() pensamento : string = 'alo alo graças a deus';
+  @Input() autor : string = 'ines brasil';
+}
