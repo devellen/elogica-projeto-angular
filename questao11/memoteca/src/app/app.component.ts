9c581adaf572fb41b0b3e06a77cb62a6c34423ba
devellen <ellenpatrycya66@gmail.com>
2025-04-16 14:46:44 -0300

    feat: implementar front pagina de form

--- questao11/memoteca/src/app/app.component.ts
+++ questao11/memoteca/src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./shared/header/header.component";
+import { FooterComponent } from "./shared/footer/footer.component";
+import { FormComponent } from "./pags/form/form.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, FooterComponent, FormComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- questao11/memoteca/src/app/pags/form/form.component.ts
+++ questao11/memoteca/src/app/pags/form/form.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from "../../shared/header/header.component";
+import { FooterComponent } from "../../shared/footer/footer.component";
+import { CardComponent } from "../../shared/card/card.component";
 
 @Component({
   selector: 'app-form',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent, FooterComponent, CardComponent],
   templateUrl: './form.component.html',
   styleUrl: './form.component.css'
 })
