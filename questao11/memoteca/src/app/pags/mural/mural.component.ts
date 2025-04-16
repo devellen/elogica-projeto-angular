a3df4bf0a4c58f419102f34187838c0f8b58f9d2
devellen <ellenpatrycya66@gmail.com>
2025-04-16 16:44:25 -0300

    feat: implementar front de pagina mural

--- questao11/memoteca/src/app/pags/mural/mural.component.ts
+++ questao11/memoteca/src/app/pags/mural/mural.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../../shared/components/header/header.component";
 import { FooterComponent } from "../../shared/components/footer/footer.component";
+import { CardComponent } from "../../shared/components/card/card.component";
 
 @Component({
   selector: 'app-mural',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent],
+  imports: [HeaderComponent, FooterComponent, CardComponent],
   templateUrl: './mural.component.html',
   styleUrl: './mural.component.css'
 })
