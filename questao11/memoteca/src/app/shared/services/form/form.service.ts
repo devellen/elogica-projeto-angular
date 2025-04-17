bbde158c4fa55a2ef0a9cf76c246e8229e0d8430
devellen <ellenpatrycya66@gmail.com>
2025-04-17 15:48:15 -0300

    feat: adicionar função de incluir pensamento

--- questao11/memoteca/src/app/shared/services/form/form.service.ts
+++ questao11/memoteca/src/app/shared/services/form/form.service.ts
@@ -1,9 +1,19 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Quote } from '../../interfaces/quote';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FormService {
 
-  constructor() { }
+    private readonly API = 'https://localhost:7192/quotes'
+
+    constructor(private httpclient: HttpClient) { }
+
+  salvarPensamento(quote : Quote) : Observable<Quote> {
+    return this.httpclient.post<Quote>(this.API, quote);
+  }
+
 }
