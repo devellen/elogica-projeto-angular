0174d9ee56148b0059f8340bcd2828d2901a0285
devellen <ellenpatrycya66@gmail.com>
2025-04-17 10:43:17 -0300

    feat: implementar a integração entre front e back, exibir lista de pensamentos

--- questao11/memoteca/src/app/pags/mural/mural.component.ts
+++ questao11/memoteca/src/app/pags/mural/mural.component.ts
@@ -1,15 +1,27 @@
-import { Component } from '@angular/core';
+import { MuralService } from './../../shared/services/mural/mural.service';
+import { Component, OnInit } from '@angular/core';
 import { HeaderComponent } from "../../shared/components/header/header.component";
 import { FooterComponent } from "../../shared/components/footer/footer.component";
 import { CardComponent } from "../../shared/components/card/card.component";
+import { Quote } from '../../shared/interfaces/quote';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-mural',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent, CardComponent],
+  imports: [HeaderComponent, FooterComponent, CardComponent, CommonModule],
   templateUrl: './mural.component.html',
   styleUrl: './mural.component.css'
 })
-export class MuralComponent {
+
+export class MuralComponent implements OnInit {
+  quotes: Quote[] = [];
+
+  constructor(private muralService: MuralService) {}
+  ngOnInit(): void {
+    this.muralService.obterPensamentos().subscribe(listaPensamentos => {
+      this.quotes = listaPensamentos;
+    })
+  }
 
 }
--- questao11/memoteca/src/app/shared/services/mural/mural.service.ts
+++ questao11/memoteca/src/app/shared/services/mural/mural.service.ts
@@ -1,9 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Quote } from '../../interfaces/quote';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class MuralService {
+  private readonly API = 'https://localhost:7192/quotes'
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  obterPensamentos(): Observable<Quote[]> {
+    return this.http.get<Quote[]>(this.API); //eniva a url
+  }
+
 }
