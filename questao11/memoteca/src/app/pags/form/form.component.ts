4eaf7383517a9e55384f693ab3eed3e1d4cc4b8c
devellen <ellenpatrycya66@gmail.com>
2025-04-17 15:42:22 -0300

    feat: implementar função de adicionar pensamento (integração com o back)

--- questao11/memoteca/src/app/pags/form/form.component.ts
+++ questao11/memoteca/src/app/pags/form/form.component.ts
@@ -1,15 +1,54 @@
-import { Component } from '@angular/core';
+import { FormService } from './../../shared/services/form/form.service';
+import { Component, OnInit } from '@angular/core';
 import { HeaderComponent } from '../../shared/components/header/header.component';
 import { FooterComponent } from '../../shared/components/footer/footer.component';
 import { CardComponent } from '../../shared/components/card/card.component';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-form',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent, CardComponent],
+  imports: [HeaderComponent, FooterComponent, CardComponent, ReactiveFormsModule],
   templateUrl: './form.component.html',
   styleUrl: './form.component.css'
 })
-export class FormComponent {
+export class FormComponent implements OnInit{
+
+  quoteForm!: FormGroup;
+
+  constructor(private formService: FormService, private router: Router, private activatedRoute: ActivatedRoute) {}
+
+  ngOnInit() {
+    this.inicializarFormulario();
+  }
+
+  inicializarFormulario() {
+    this.quoteForm = new FormGroup({
+      pensamento: new FormControl(''),
+      modelo: new FormControl(''),
+      autor: new FormControl(''),
+
+    })
+  }
+
+  salvarQuote() {
+    const novoQuote = this.quoteForm.value;
+    this.formService.salvarPensamento(novoQuote).subscribe(
+      () => {
+      this.quoteForm.reset();
+      this.router.navigateByUrl('/mural');
+
+            // {
+      //   next: response => {
+      //     console.log(response)
+      //   },
+      //   error: err => {console.log('erro', err)}
+      // }
+    }
+  );
+
+
+  }
 
 }
